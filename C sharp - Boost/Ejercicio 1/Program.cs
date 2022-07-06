1d3e625483b65a130f93c9c47b16f9e160dd03fa
arozas <alejandro.rozas@gmail.com>
2022-07-06 18:57:49 -0300

    Ejercicios C# 1 y 2 Terminados

---
+++ C sharp - Boost/Ejercicio 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hola, dime tu nombre por favor:");
+string nameInput = Console.ReadLine();
+Console.WriteLine("Es un placer conocerte, " + nameInput);
---
+++ C sharp - Boost/Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("El día y hora actual es:");
+Console.WriteLine(DateTime.Now);
