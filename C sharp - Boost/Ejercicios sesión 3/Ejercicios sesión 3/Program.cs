c276ac4e900774fa8df3f281863b86a57c91bd7f
arozas <alejandro.rozas@gmail.com>
2022-07-07 15:46:51 -0300

    Ejercicios Ejercicios sesión 3 Terminados

---
+++ C sharp - Boost/Ejercicios sesión 3/Ejercicios sesión 3/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("EJERCICIOS SESIÓN 3");
+
+Client newClient = new Client("Alejandro", "Rozas","15-6275-5583","alejandro.rozas@gmail.com",true);
+
+Console.WriteLine(newClient);
+
+public struct Client
+{
+    public Client (string name, string surnname, string phone,string email, bool newClient)
+    {
+        Name = name;
+        Surname = surnname;
+        Phone = phone;
+        Email = email;
+        NewClient = newClient;
+    }
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    [DataType(DataType.PhoneNumber)]
+    public string Phone { get; set; }
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    public bool NewClient { get; set; }
+    
+    public override string ToString() => $"{Name},{Surname},{Phone},{Email},{NewClient}";
+}
---
+++ C sharp - Boost/Pruebas/Estructuras/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("PRUEBAS DE ESTRUCTURAS");
+
+//cada vea que se crea un objeto este debe ser inicializado, para eso tenemos el comando new.
+//no puede en orden secuencial la declaración del objeto estar debajo de la clase.
+//"Las instruciones de nivel superior deben preceder a las declaraciones de espacios de nombre y tipos"
+Coords myCoords = new Coords(2,10,0); 
+
+Console.WriteLine("Valor X: " + myCoords.X);
+Console.WriteLine("Valor Y: " + myCoords.Y);
+Console.WriteLine("Valor Z: " + myCoords.Z);
+
+//estoy clonando la estructura (objeto) y declarando un nuevo valor a uno de sus atributos.
+Coords myCoordsTwo = myCoords with { X  = 3 };
+Console.WriteLine(myCoordsTwo);
+public struct Coords
+{
+    public Coords (double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    //Si solo declaramos el get vamos solamente poder leer el valor
+    // para modificar el valor debemos declarar los set también.
+    public double X { get; set; } 
+    public double Y { get; set; }
+    public double Z { get; set; }
+    
+    public override string ToString() => $"({X},{Y}),{Z}";
+}
